6983c18876ad007a76bbecf397526bd8d07aed58
William Bogans <william.bogans@students.makeschool.com>
2019-02-02 14:14:51 -0800

    Setup a basic header

--- scholarboard/src/App.js
+++ scholarboard/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './Styles/Global.css';
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Header />
       </div>
     );
   }
---
+++ scholarboard/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../Styles/Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Hello From the Header</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
