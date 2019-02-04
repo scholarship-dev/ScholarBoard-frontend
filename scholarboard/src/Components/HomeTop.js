6cbae102d49c8adf8e00ac52f725eecf16bdf360
Asim Zaidi <asimzaidih@gmail.com>
2019-02-04 02:17:27 -0800

    Typed.js React Wrapper :package: on HomePage

--- scholarboard/src/Components/HomeTop.js
+++ scholarboard/src/Components/HomeTop.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ScholarshipImg from '../Images/scholarship.png';
 import { Link } from 'react-router-dom';
-
+import Typed from 'react-typed';
 
 const HomeTop = () => {
   return (
     <section className='home-top'>
       <div className="info-section">
-        <h3 className="slogan"><span role="img" aria-label="Grad Icon">🎓</span> Scholarships made easy for high schoolers</h3>
+        <h3 className="slogan"><span role="img" aria-label="Grad Icon">🎓</span> <Typed strings={['Scholarships made easy for high schoolers']} typeSpeed={20} /></h3>
         <p className="top-sentence">ScholarBoard allows students to fill out a common application that can be used to apply to dozens of scholarships relevent to them with a click of a button.</p>
         <Link to='/signup'><button className="btn">Sign up</button></Link>
         <p className="credits">Created by <span>Asim</span>, <span>Medi</span>, <span>Thomas</span>, and <span>William</span>.</p>
