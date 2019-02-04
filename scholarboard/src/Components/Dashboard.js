9c48276e4af7f079e6435135a23ad45f2517eb29
Asim Zaidi <asimzaidih@gmail.com>
2019-02-04 02:42:42 -0800

    Signup Search Tool Style

--- scholarboard/src/Components/Dashboard.js
+++ scholarboard/src/Components/Dashboard.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-
-
-
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Typed from 'react-typed';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Dashboard</h1>
-      </div>
-    )
+          <Navbar />
+          <div className="dashboard-page">
+            <h1 className="db-title"><Typed strings={['Scholarships For You, Jeremy']} typeSpeed={16} /></h1>
+            <form className="sort-tool btn-shadow">
+            <input placeholder="Search Term"></input>
+              <select name="Filters">
+                <option value="1">Filter #1</option>
+                <option value="2">Filter #2</option>
+                <option value="3">Filter #3</option>
+                <option value="4">Filter #4</option>
+              </select>
+
+              <button className="btn">Search</button>
+            </form>
+          </div>
+          <Footer />
+        </div>
+    );
   }
 }
 
