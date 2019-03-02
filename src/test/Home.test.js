bfaab99c5f3c9310ea421964e527b8b68ceb9eb7
William Bogans <william.bogans@students.makeschool.com>
2019-03-01 16:10:04 -0800

    [Working] - just added user component testing

--- src/test/Home.test.js
+++ src/test/Home.test.js
@@ -1,9 +1,27 @@
-import App from '../App';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Features from './components/HomePageComps/Features';
 import Header from './components/HomePageComps/Header';
+import Team from './components/HomePageComps/Team';
 
 
+it('renders Header Component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('renders Features component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Features />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('renders Team component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Team />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
 
 it('renders Footer Component without crashing', () => {
   const div = document.createElement('div');
@@ -11,9 +29,3 @@ it('renders Footer Component without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 })
 
-
-it('renders Features component without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Features />, div);
-  ReactDOM.unmountComponentAtNode(div);
-})
---
+++ src/test/User.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dashboard from './components/UserComps/Dashboard';
+import Scholarship from './components/UserComps/Scholarship';
+
+
+
+
+it('renders Dashboard Component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Dashboard />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+
+it('renders Scholarship component without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Scholarship />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
