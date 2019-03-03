257f7091a08f1d222a21f51c9745ecdf2adcd3d7
William Bogans <william.bogans@students.makeschool.com>
2019-03-02 17:26:13 -0800

    [Working] - starting to setup the basic structure for the header

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./js/store";
 import './styles/css/main.css';
-// import Home from './components/HomePageComps/Home';
+import Navbar from './components/GeneralComps/Navbar';
+import Home from './components/HomePageComps/Home';
 // import Login from './components/AuthPageComps/Login';
 // import Signup from './components/AuthPageComps/Signup';
 // import Dashboard from './components/UserComps/Dashboard';
-import Navbar from './components/GeneralComps/Navbar';
+
 // import Footer from './components/GeneralComps/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Route path='/' component={Navbar} />
-            {/* <Route path='/' exact component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={Signup} />
-            <Route exact path='/dashboard' component={Dashboard} /> */}
-            {/* <Route path='/' component={Footer} /> */}
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+
+        <Route path="/" component={Navbar} />
+        <Route path="/" exact component={Home} />
+
+
+        {/* <Route exact path='/login' component={Login} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/dashboard' component={Dashboard} /> */}
+        {/* <Route path='/' component={Footer} /> */}
+
+      </div>
+    </Router>
+  </Provider>
+);
 
 export default App;
