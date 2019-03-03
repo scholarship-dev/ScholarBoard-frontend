e012c9c70e1d074dbe43b782b7c0bdafdd5027a5
William Bogans <william.bogans@students.makeschool.com>
2019-03-02 17:32:21 -0800

    [Working] - setup basic header

--- src/Components/HomePageComps/Header.js
+++ src/Components/HomePageComps/Header.js
@@ -5,22 +5,23 @@ import ScholarshipImg from '../../images/headerlogo.png';
 
 const Header = () => (
     <header>
-        <div>
-            <h3>
-                <span role="img" aria-label="Grad Icon">🎓</span>
-                <Typed strings={['Scholarship search made easy for students.']} typeSpeed={20} />
-            </h3>
 
-            <p className="top-sentence">ScholarBoard allows students to fill out a common application that can be used to apply to dozens of scholarships relevent to them with a click of a button.</p>
+        <span role="img" aria-label="Grad Icon">
+            🎓
+            </span>
+        <h3>
+            <Typed
+                strings={['Scholarship search made easy for students.']} typeSpeed={20}
+            />
+        </h3>
 
-            <Link to="/signup">
-                <button className="btn">Sign up</button>
-            </Link>
+        <p className="top-sentence">ScholarBoard allows students to fill out a common application that can be used to apply to dozens of scholarships relevent to them with a click of a button.</p>
 
-        </div>
-        <div className="img-section">
-            <img className="scholarship-img" src={ScholarshipImg} alt="Scholarship" />
-        </div>
+        <Link to="/signup">
+            <button className="btn">Sign up</button>
+        </Link>
+
+        <img className="scholarship-img" src={ScholarshipImg} alt="Scholarship" />
     </header>
 );
 
