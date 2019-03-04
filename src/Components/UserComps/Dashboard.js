5af46b017daaa5696f8419ce6009f217fb9c8244
William Bogans <william.bogans@students.makeschool.com>
2019-03-03 22:47:38 -0800

    [Working] - refactoring the dashboard

--- src/Components/UserComps/Dashboard.js
+++ src/Components/UserComps/Dashboard.js
@@ -3,78 +3,38 @@ import Typed from 'react-typed';
 import axios from 'axios';
 
 class Dashboard extends Component {
-
-  componentDidMount() {
-    axios.get(`/api/scholarships`).then(data => {
-      console.log("data:", data);
-    }).catch(console.err);
-  };
-
-
+  constructor(props) {
+    super(props)
+  }
 
   render() {
     return (
-      <div className="dashboard-page">
-        <h1 className="db-title"><Typed strings={['Scholarships For You, Jeremy']} typeSpeed={16} /></h1>
-        <form className="sort-tool btn-shadow">
-          <input placeholder="Search Term"></input>
-          <select name="Filters">
-            <option value="1">Filter #1</option>
-            <option value="2">Filter #2</option>
-            <option value="3">Filter #3</option>
-            <option value="4">Filter #4</option>
-          </select>
+      <div className="dashboard">
 
-          <button className="btn">Search</button>
-        </form>
+        <section>
 
-        <div className="s-card btn-shadow">
-          <div className="left">
-            <p className="title">Scholarship Title Goes Here</p>
-            <p className="description">lorep de ipsum de lorep de ipsum de lorep de ipsum dev lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de </p>
-          </div>
-          <div className="right">
-            <p className="date-text">Apply By</p>
-            <p>NOV O7</p>
-            <button className="btn">Read More</button>
-          </div>
-        </div>
+          <nav>
 
-        <div className="s-card btn-shadow">
-          <div className="left">
-            <p className="title">Scholarship Title Goes Here</p>
-            <p className="description">lorep de ipsum de lorep de ipsum de lorep de ipsum dev lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de </p>
-          </div>
-          <div className="right">
-            <p className="date-text">Apply By</p>
-            <p>NOV O7</p>
-            <button type="submit" className="btn">Read More</button>
-          </div>
-        </div>
+          </nav>
 
-        <div className="s-card btn-shadow">
-          <div className="left">
-            <p className="title">Scholarship Title Goes Here</p>
-            <p className="description">lorep de ipsum de lorep de ipsum de lorep de ipsum dev lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de </p>
-          </div>
-          <div className="right">
-            <p className="date-text">Apply By</p>
-            <p>NOV O7</p>
-            <button className="btn">Read More</button>
-          </div>
-        </div>
+        </section>
 
-        <div className="s-card btn-shadow">
-          <div className="left">
-            <p className="title">Scholarship Title Goes Here</p>
-            <p className="description">lorep de ipsum de lorep de ipsum de lorep de ipsum dev lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de lorep de ipsum de </p>
-          </div>
-          <div className="right">
-            <p className="date-text">Apply By</p>
-            <p>NOV O7</p>
-            <button className="btn">Read More</button>
-          </div>
-        </div>
+
+        <section>
+          <h1 className="db-title"><Typed strings={['Scholarships For You, Jeremy']} typeSpeed={16} /></h1>
+          <form className="sort-tool btn-shadow">
+            <input placeholder="Search Term"></input>
+            <select name="Filters">
+              <option value="1">Filter #1</option>
+              <option value="2">Filter #2</option>
+              <option value="3">Filter #3</option>
+              <option value="4">Filter #4</option>
+            </select>
+
+            <button className="btn">Search</button>
+          </form>
+        </section>
+
 
       </div>
     );
