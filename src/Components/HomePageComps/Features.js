d729de5fcb4599faca4f16257cdcc9a9b401bcc0
William Bogans <william.bogans@students.makeschool.com>
2019-03-05 15:12:56 -0800

    [Working] - refactored the features into their own components

--- src/Components/HomePageComps/Features.js
+++ src/Components/HomePageComps/Features.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Feature from './Feature';
 
 const Features = () => (
   <div className="Features page-section">
@@ -7,44 +8,45 @@ const Features = () => (
 
       <div className="feature-step signup">
         <section className="feature-step-content">
-          <h4>Step 1. Sign Up</h4>
+          <Feature
+            step="Step 1. Sign Up"
+            description="We use the data you provide in the sign up page to
+              scrape the web for relevant scholarships that match your profile."
+          />
+        </section>
+      </div>
 
-          <p>
-            Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit.
-            Quia dolores molestias
-            vitae corrupti esse sunt nemo
-            debitis tempora ex quo!
-          </p>
-
+      <div className="feature-step apply">
+        <section className="feature-step-content">    
+          <Feature
+            step="Step 2. Your Dashboard"
+            description="You can view all the scholarships that
+            match your description beautifully placed in your dashboard"
+          />
         </section>
       </div>
 
       <div className="feature-step apply">
         <section className="feature-step-content">
-
-          <h4>Step 2. Apply</h4>
-          <p>
-            Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit.
-            Quia dolores molestias
-            vitae corrupti esse sunt nemo
-            debitis tempora ex quo!
-          </p>
+          <Feature
+            step="Step 3. Apply"
+            description="You can click on each scholarships to apply to them directly.
+            No need to examine if it fits you, our algorithm does it for you!"
+          />
         </section>
       </div>
 
+
       <div className="feature-step celebrate">
         <section className="feature-step-content">
-
-          <h4>Step 3. Celebrate</h4>
-          <p>
-            Lorem ipsum dolor,
+          <Feature
+            step="Step 4. Celebrate"
+            description="  Lorem ipsum dolor,
             sit amet consectetur adipisicing elit.
             Quia dolores molestias
             vitae corrupti esse sunt nemo
-            debitis tempora ex quo!
-          </p>
+            debitis tempora ex quo!"
+          />
         </section>
       </div>
 
