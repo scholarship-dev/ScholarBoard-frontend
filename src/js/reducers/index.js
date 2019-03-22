34ed3dff7c60291d31e5e31a4ec4068ea7a52184
William Bogans <williambogans98@yahoo.com>
2019-03-21 23:53:36 -0700

    cleaned up the auth redux

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -10,13 +10,7 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
-    case HANDLE_LOGIN:
-      return { ...state, user: action.payload };
     case SIGNUP_USER:
-      return { ...state, user: action.payload };
-    case LOGOUT_USER:
-      return { ...state, user: '' };
-    case HANDLE_SETTINGS:
       return { ...state, user: action.payload };
     default:
       return state;
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -3,7 +3,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-// import { forbiddenWordsMiddleware } from "../middlewares";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
