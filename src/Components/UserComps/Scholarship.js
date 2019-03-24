5c810d4ecc80243792f3755a1cac0a63a9a44f3f
William Bogans <williambogans98@yahoo.com>
2019-03-24 16:52:36 -0700

    changing the slicing values

--- src/Components/UserComps/Scholarship.js
+++ src/Components/UserComps/Scholarship.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Scholarship = ({ name, description, id, funding }) => (
   <div className="scholarship" key={id}>
     <h1>{name}</h1>
-    <p>{description}</p>
+    <p>{description.slice(0, 200) + '...'}</p>
     <p>Funding: {funding}</p>
   </div>
 );
