5bc38b464e5da6c4908c8ceacde9c6fb449cc635
William Bogans <williambogans98@yahoo.com>
2019-03-23 22:21:55 -0700

    header items in the right order, just need to center them

--- src/Components/HomePageComps/Header.js
+++ src/Components/HomePageComps/Header.js
@@ -6,7 +6,7 @@ import headerPeople from '../../images/home-page-pics/header-people.png';
 const Header = () => (
     <header>
 
-        <section className="cta-content">
+        <section className="cta-content a">
             <h1>
                 <Typed
                     strings={['Scholarship search made easy for students.']} typeSpeed={20}
@@ -17,12 +17,17 @@ const Header = () => (
                 ScholarBoard allows students to fill out a common application that can be used to apply to dozens of scholarships relevant to them with a click of a button.
             </p>
 
-            <Link to="/signup">
+
+
+        </section>
+        <div className="button">
+            <Link to="/signup" className="b">
                 <button className="btn">Sign up</button>
             </Link>
-        </section>
+        </div>
 
-        <img src={headerPeople} alt="svg of  people" />
+
+        <img className="c" src={headerPeople} alt="svg of  people" />
 
     </header>
 );
