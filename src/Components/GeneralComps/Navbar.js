54fe2640f26bc5992154a087299c1b495bca2c0d
William Bogans <williambogans98@yahoo.com>
2019-03-24 00:53:15 -0700

    done with the landing page!

--- src/Components/GeneralComps/Navbar.js
+++ src/Components/GeneralComps/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/home-page-pics/header-logo.png';
+import whiteLogo from '../../images/home-page-pics/white-header-logo.png';
 
 const Navbar = () => (
   <nav>
@@ -10,13 +11,18 @@ const Navbar = () => (
         className="header-logo"
         src={logo}
         alt="Logo"
-        srcSet=""
+      />
+      <img src={whiteLogo}
+        alt="Logo"
+        className="white-header-logo"
       />
     </Link>
 
     <div className="nav-right">
+
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
+
       <i className="fas fa-bars"></i></div>
 
   </nav>
