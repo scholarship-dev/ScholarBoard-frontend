125661b5bcc66aa29f0541fdb92c326b90db2dd5
William Bogans <williambogans98@yahoo.com>
2019-03-23 17:55:33 -0700

    feature section is coming along

--- src/Components/HomePageComps/Feature.js
+++ src/Components/HomePageComps/Feature.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Feature = ({ step, description, img, alt }) => (
-  <>
+  <div className="feature">
     <img src={img} alt={alt} />
     <h4>{step}</h4>
     <p>{description}</p>
-  </>
+  </div>
 );
 
 
--- src/Components/HomePageComps/Features.js
+++ src/Components/HomePageComps/Features.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import signUp from '../../images/home-page-pics/signup.jpg';
+import dashboard from '../../images/home-page-pics/dashboard.jpg';
+import apply from '../../images/home-page-pics/apply.jpg';
+import party from '../../images/home-page-pics/party.jpg';
+
+
 import Feature from './Feature';
 
 const Features = () => (
@@ -9,7 +14,6 @@ const Features = () => (
     <section className="feature-section">
 
           <Feature
-          
               img={signUp}
               alt="The image for signup"
               step="Step 1. Sign Up"
@@ -17,35 +21,32 @@ const Features = () => (
               scrape the web for relevant scholarships that match your profile."
           />
 
-      {/* <div className="feature-step dashboard">
-        <section className="feature-step-content d">
           <Feature
             step="Step 2. Your Dashboard"
             description="You can view all the scholarships that
             were curated just for you beautifully placed in your dashboard."
+            img={dashboard}
+            alt="An image of a dashboard"
           />
-        </section>
-      </div>
 
-      <div className="feature-step apply">
-        <section className="feature-step-content">
           <Feature
             step="Step 3. Apply"
             description="You can click on each scholarship to apply to them directly.
             No need to examine if it fits you, our algorithm does it for you!"
+            img={apply}
+            alt="Applying for a scholarship"
           />
-        </section>
-      </div>
 
 
-      <div className="feature-step celebrate">
-        <section className="feature-step-content">
+
+
           <Feature
             step="Step 4. Celebrate"
             description="Give yourself a pat on the back! By using Scholarboard you have saved thousands of hours searching for scholarships."
+            img={party}
+            alt="Celebrate"
           />
-        </section>
-      </div> */}
+
       </section>
   </div>
 );
