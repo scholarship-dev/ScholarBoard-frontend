b0a68069d41f0202afae710c5e91aba6cdd0fdb3
William Bogans <williambogans98@yahoo.com>
2019-03-24 18:02:46 -0700

    added a button to the scholarship

--- src/Components/UserComps/Scholarship.js
+++ src/Components/UserComps/Scholarship.js
@@ -7,7 +7,10 @@ const Scholarship = ({ name, description, id, funding }) => (
   <div className="scholarship" key={id}>
     <h1>{name}</h1>
     <p>{description.slice(0, 150) + '...'}</p>
-    <p>Funding: {funding}</p>
+    <section className="scholarship-bottom">
+      <p>Funding: {funding}</p>
+      <button>View Details</button>
+    </section>
   </div>
 );
 
