b7d1125a33558b4bf36f2d5682b6afb3d34aa4a2
William Bogans <williambogans98@yahoo.com>
2019-03-24 17:04:14 -0700

    styling the cards

--- src/Components/UserComps/Scholarship.js
+++ src/Components/UserComps/Scholarship.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+
+
+
 const Scholarship = ({ name, description, id, funding }) => (
   <div className="scholarship" key={id}>
     <h1>{name}</h1>
-    <p>{description.slice(0, 200) + '...'}</p>
+    <p>{description.slice(0, 300) + '...'}</p>
     <p>Funding: {funding}</p>
   </div>
 );
